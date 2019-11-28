e580de3d0f24d910fa7cbd49bca7d088bac8b0c3
Jakub Jatczak <jjatczak@antmicro.com>
2019-11-28 16:09:05 +0100

    Make osx first and remove doubled code from conda-get

--- conda-get.sh
+++ conda-get.sh
@@ -16,16 +16,10 @@ fi
 wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-${sys_name}-x86_64.sh
 chmod a+x Miniconda3-latest-${sys_name}-x86_64.sh
 if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
+	echo "Installing conda"
         ./Miniconda3-latest-${sys_name}-x86_64.sh -p $CONDA_PATH -b -f
 fi
 
-wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod a+x Miniconda3-latest-Linux-x86_64.sh
-
-if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
-	echo "Installing conda"
-        ./Miniconda3-latest-Linux-x86_64.sh -p $CONDA_PATH -b -f
-fi
 export PATH=$CONDA_PATH/bin:$PATH
 
 # Updating conda
