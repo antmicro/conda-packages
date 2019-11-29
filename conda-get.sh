7b42653d0c59a1b8ef884db34f1541af2b4e0d55
Jakub Jatczak <jjatczak@antmicro.com>
2019-11-29 14:37:35 +0100

    Modify .tarvis configuration files and conda-* scripts to work on windows

--- conda-get.sh
+++ conda-get.sh
@@ -6,22 +6,34 @@ set -e
 CONDA_PATH=${1:-~/conda}
 
 echo "Downloading Conda installer."
-
-if [ $TRAVIS_OS_NAME = 'linux' ]; then
-    sys_name=Linux
+if [ $TRAVIS_OS_NAME = 'windows' ]; then
+    if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
+        wget -c https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -o miniconda.exe
+        echo "Installing conda"
+        nohup powershell ./miniconda.exe "//S //D=$CONDA_PATH"
+    fi
+    alias conda='powershell $CONDA_PATH/condabin/conda'
 else
-    sys_name=MacOSX
-fi
+    if [ $TRAVIS_OS_NAME = 'linux' ]; then
+        sys_name=Linux
+    else
+        sys_name=MacOSX
+    fi
 
-wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-${sys_name}-x86_64.sh
-chmod a+x Miniconda3-latest-${sys_name}-x86_64.sh
-if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
-	echo "Installing conda"
-        ./Miniconda3-latest-${sys_name}-x86_64.sh -p $CONDA_PATH -b -f
+    wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-${sys_name}-x86_64.sh
+    chmod a+x Miniconda3-latest-${sys_name}-x86_64.sh
+    if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
+            echo "Installing conda"
+            ./Miniconda3-latest-${sys_name}-x86_64.sh -p $CONDA_PATH -b -f
+    fi
 fi
 
 export PATH=$CONDA_PATH/bin:$PATH
 
 # Updating conda
 conda update -y conda setuptools
-conda install -y conda-build anaconda-client jinja2 conda-verify ripgrep
+if [ $TRAVIS_OS_NAME = 'windows' ]; then
+    conda install -y conda-build anaconda-client jinja2 conda-verify
+else
+    conda install -y conda-build anaconda-client jinja2 conda-verify ripgrep
+fi
