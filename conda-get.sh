cbeabab04a633e05aa4d57f95d4422b216f6b1d9
Jan Kowalewski <jkowalewski@antmicro.com>
2020-04-20 18:16:51 +0200

    check version

--- conda-get.sh
+++ conda-get.sh
@@ -59,6 +59,7 @@ cat >> $CONDA_PATH/condarc <<'EOF'
 EOF
 
 # Install required build tools
-conda install -y conda-build=3.14 anaconda-client jinja2 conda-verify ripgrep pexpect
+conda install -y conda-build anaconda-client jinja2 conda-verify ripgrep pexpect
+conda build --version
 
 conda info | grep --color=always -e "^" -e "populated config files"
