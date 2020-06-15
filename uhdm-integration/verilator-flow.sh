a074c5c4f1661a30737b9ce2a2d3b228dd1fb01f
Tomasz Gorochowik <tgorochowik@antmicro.com>
2020-06-15 11:28:22 +0200

    uhdm-integration: generate three separate packages
    For surelog-uhdm, verilator-uhdm and yosys-uhdm.
    
    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>

--- uhdm-integration/verilator-flow.sh
+++ uhdm-integration/verilator-flow.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-sed -i 's/"verilator_bin"/"uhdm-verilator_bin"/g' image/bin/verilator
-sed -i 's/"verilator_bin_dbg"/"uhdm-verilator_bin_dbg"/g' image/bin/verilator
-
 mkdir -p "$PREFIX/bin"
-mkdir -p "$PREFIX/lib/surelog/sv"
 
-cp "$SRC_DIR/image/lib/surelog/sv/builtin.sv" "$PREFIX/lib/surelog/sv/builtin.sv"
-cp "$SRC_DIR/image/bin/surelog" "$PREFIX/bin/uhdm-surelog"
-cp "$SRC_DIR/Surelog/build/dist/Release/hellosureworld" "$PREFIX/bin/uhdm-hellosureworld"
-cp "$SRC_DIR/image/bin/verilator" "$PREFIX/bin/uhdm-verilator"
-cp "$SRC_DIR/image/bin/verilator_bin" "$PREFIX/bin/uhdm-verilator_bin"
-cp "$SRC_DIR/image/bin/verilator_bin_dbg" "$PREFIX/bin/uhdm-verilator_bin_dbg"
+sed -i 's/"verilator_bin"/"verilator_bin-uhdm"/g' image/bin/verilator
+sed -i 's/"verilator_bin_dbg"/"verilator_bin_dbg-uhdm"/g' image/bin/verilator
+
+cp "$SRC_DIR/image/bin/verilator" "$PREFIX/bin/verilator-uhdm"
+cp "$SRC_DIR/image/bin/verilator_bin" "$PREFIX/bin/verilator_bin-uhdm"
+cp "$SRC_DIR/image/bin/verilator_bin_dbg" "$PREFIX/bin/verilator_bin_dbg-uhdm"
--- uhdm-integration/yosys-flow.sh
+++ uhdm-integration/yosys-flow.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 mkdir -p "$PREFIX/bin"
-mkdir -p "$PREFIX/lib/surelog/sv"
 
-cp "$SRC_DIR/image/lib/surelog/sv/builtin.sv" "$PREFIX/lib/surelog/sv/builtin.sv"
-cp "$SRC_DIR/image/bin/surelog" "$PREFIX/bin/uhdm-surelog"
-cp "$SRC_DIR/Surelog/build/dist/Release/hellosureworld" "$PREFIX/bin/uhdm-hellosureworld"
-cp "$SRC_DIR/yosys/yosys" "$PREFIX/bin/uhdm-yosys"
+cp "$SRC_DIR/yosys/yosys" "$PREFIX/bin/yosys-uhdm"
