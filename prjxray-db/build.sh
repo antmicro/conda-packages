e880c96d90d577ed54fd708a398cef90ec1b4857
Alessandro Comodi <acomodi@antmicro.com>
2020-07-13 19:42:23 +0200

    prjxray-db: add script to bin that gets the db location
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- prjxray-db/build.sh
+++ prjxray-db/build.sh
@@ -15,3 +15,10 @@ mkdir -p $DBDIR
 for device in $DEVICES; do
     cp -r $device $DBDIR
 done
+
+mkdir -p $PREFIX/bin
+
+DB_CONFIG="${PREFIX}/bin/prjxray-config"
+
+echo -e "#!/bin/bash\n\necho ${DBDIR}" > ${DB_CONFIG}
+chmod +x ${DB_CONFIG}
