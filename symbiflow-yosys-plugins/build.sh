ec6b707b794d04be3d2229d695dffd83061951a6
Tomasz Michalak <tmichalak@antmicro.com>
2020-09-16 09:30:05 +0200

    yosys-plugins: Install all plugins with new SDC plugin
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- symbiflow-yosys-plugins/build.sh
+++ symbiflow-yosys-plugins/build.sh
@@ -11,7 +11,4 @@ which pkg-config
 
 echo "PREFIX := $PREFIX" >> Makefile.conf
 
-make -C fasm-plugin install -j$CPU_COUNT
-make -C xdc-plugin install -j$CPU_COUNT
-make -C params-plugin install -j$CPU_COUNT
-make -C selection-plugin install -j$CPU_COUNT
+make install -j$CPU_COUNT
