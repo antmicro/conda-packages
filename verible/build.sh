106933047f78ab87673fef09011fc9589c4ed7e9
Pawel Czarnecki <pczarnecki@antmicro.com>
2020-12-30 14:35:24 +0100

    try most recent bazel version
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- verible/build.sh
+++ verible/build.sh
@@ -10,11 +10,10 @@ export CXX=g++-${USE_SYSTEM_GCC_VERSION}
 mkdir bazel-install
 BAZEL_PREFIX=$PWD/bazel-install
 
-wget https://github.com/bazelbuild/bazel/releases/download/0.29.1/bazel-0.29.1-installer-linux-x86_64.sh
-chmod +x bazel-0.29.1-installer-linux-x86_64.sh
-./bazel-0.29.1-installer-linux-x86_64.sh --prefix=$BAZEL_PREFIX
+wget https://github.com/bazelbuild/bazel/releases/download/3.7.2/bazel-3.7.2-installer-linux-x86_64.sh
+chmod +x bazel-3.7.2-installer-linux-x86_64.sh
+./bazel-3.7.2-installer-linux-x86_64.sh --prefix=$BAZEL_PREFIX
 
 export PATH=$BAZEL_PREFIX/bin:$PATH
 
 bazel run :install -c opt -- $PREFIX/bin
-
